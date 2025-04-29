94f5c08cdda2d086d586713d2fb153d6d22c9585
AGachelin <159674898+AGachelin@users.noreply.github.com>
2025-04-29 17:27:23 +0200

    fixed base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://agachelin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tpiot-doc',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
